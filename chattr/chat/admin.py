476135f4790670957f1a622251debd99ccd9bdc4
lex <lex@users.noreply.github.com>
2022-09-28 01:16:06 +0300

    add models for channels and messages

--- chattr/chat/admin.py
+++ chattr/chat/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Channel, Message
+
+admin.site.register(Channel)
+admin.site.register(Message)
--- chattr/chat/models.py
+++ chattr/chat/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Channel(models.Model):
+    name = models.CharField(max_length=64)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+
+
+class Message(models.Model):
+    username = models.CharField(max_length=64, editable=False)
+    message = models.CharField(max_length=1024, editable=False)
+    channel = models.ForeignKey(
+        Channel, related_name='messages', on_delete=models.CASCADE, editable=False)
+    timestamp = models.DateTimeField(auto_now_add=True, editable=False)
+
+    def __str__(self):
+        return '{} - {}: {}'.format(self.channel, self.username, self.message)
+
+    class Meta:
+        ordering = ('timestamp',)
