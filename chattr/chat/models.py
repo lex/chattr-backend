f89bfb215450a4ae6ba77e1529ad00f69842a381
lex <lex@users.noreply.github.com>
2022-09-28 01:53:42 +0300

    remove editable=False from Message fields

--- chattr/chat/models.py
+++ chattr/chat/models.py
@@ -13,11 +13,11 @@ class Channel(models.Model):
 
 
 class Message(models.Model):
-    username = models.CharField(max_length=64, editable=False)
-    message = models.CharField(max_length=1024, editable=False)
+    username = models.CharField(max_length=64)
+    message = models.CharField(max_length=1024)
     channel = models.ForeignKey(
-        Channel, related_name='messages', on_delete=models.CASCADE, editable=False)
-    timestamp = models.DateTimeField(auto_now_add=True, editable=False)
+        Channel, related_name='messages', on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return '{} - {}: {}'.format(self.channel, self.username, self.message)
