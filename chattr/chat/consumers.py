fffebe1fac993a2835842fa9616359d5cd73d098
lex <lex@users.noreply.github.com>
2022-09-28 01:05:09 +0300

    name -> username, get rid of message types

--- chattr/chat/consumers.py
+++ chattr/chat/consumers.py
@@ -4,9 +4,7 @@ from asgiref.sync import async_to_sync
 
 # pretty easy to add support for more chat channels?
 GROUP_NAME = 'chat'
-MESSAGE_TYPE_NEW_MESSAGE = 'new_message'
-MESSAGE_TYPE_CHATTER_JOINED = 'new_chatter'
-MESSAGE_TYPE_UNDEFINED = 'undefined_message_type'
+EVENT_TYPE_NEW_MESSAGE = 'new_message'
 
 
 class ChatConsumer(JsonWebsocketConsumer):
@@ -24,45 +22,28 @@ class ChatConsumer(JsonWebsocketConsumer):
         )
 
     def receive_json(self, content):
-        message_type = content['message_type']
-        name = content['name']
-        message = content['message']
+        username = content.get('username', 'undefined')
+        message = content.get('message', 'undefined')
         timestamp = datetime.now(
             timezone.utc).isoformat().replace("+00:00", "Z")
 
         async_to_sync(self.channel_layer.group_send)(
             GROUP_NAME,
             {
-                'type': MESSAGE_TYPE_NEW_MESSAGE if message_type == MESSAGE_TYPE_NEW_MESSAGE else
-                (MESSAGE_TYPE_CHATTER_JOINED if message_type ==
-                 MESSAGE_TYPE_CHATTER_JOINED else MESSAGE_TYPE_UNDEFINED),
-                'name': name,
+                'type': EVENT_TYPE_NEW_MESSAGE,
+                'username': username,
                 'message': message,
                 'timestamp': timestamp,
             }
         )
 
     def new_message(self, event):
-        name = event['name']
+        username = event['username']
         message = event['message']
         timestamp = event['timestamp']
 
         self.send_json({
-            'message_type': MESSAGE_TYPE_NEW_MESSAGE,
-            'name': name,
+            'username': username,
             'message': message,
             'timestamp': timestamp,
         })
-
-    def new_chatter(self, event):
-        name = event['name']
-        timestamp = event['timestamp']
-
-        self.send_json({
-            'message_type': MESSAGE_TYPE_CHATTER_JOINED,
-            'name': name,
-            'timestamp': timestamp,
-        })
-
-    def undefined_message_type(self, event):
-        print('something went wrong')
