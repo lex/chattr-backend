9d74f1e5dd6c1611f5b4c8fb03d5e36a273113b4
lex <lex@users.noreply.github.com>
2022-09-28 21:29:26 +0300

    add a simple channel listing as json

--- chattr/chat/urls.py
+++ chattr/chat/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('list/', views.channel_list, name='channel_list'),
     path('channel/<int:channel_id>/', views.channel, name='channel'),
 ]
--- chattr/chat/views.py
+++ chattr/chat/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import Channel
 
 
 def index(request):
     return render(request, 'index.html', {'channels': Channel.objects.all()})
+
+def channel_list(request):
+    return JsonResponse(list(Channel.objects.all().values()), safe=False)
 
 
 def channel(request, channel_id):
